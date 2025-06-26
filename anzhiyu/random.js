c6b43e1ec4169b820d6aab00b6f6324deae6de55
徐彤彤 <xutongtong@legendapl.com.cn>
2025-06-26 15:48:17 +0800

    Site updated: 2025-06-26 15:48:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2013/07/13/hello-world/","2024/07/13/generator/"];function toRandomPost(){
+var posts=["2024/07/13/generator/","2013/07/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
