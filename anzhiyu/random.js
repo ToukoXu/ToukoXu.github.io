3a5d03f560876afcfbaf9671b17bd8069e09fd1f
徐彤彤 <xutongtong@legendapl.com.cn>
2025-11-07 10:32:00 +0800

    Site updated: 2025-11-07 10:31:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["archives/async+await/","archives/closure/","archives/event/","archives/iterator/","archives/generator/","archives/message-channel/","archives/prototype-chain/","archives/proxy/","archives/reflect/","archives/scope-chain/"];function toRandomPost(){
+var posts=["archives/async+await/","archives/closure/","archives/event/","archives/generator/","archives/message-channel/","archives/iterator/","archives/prototype-chain/","archives/reflect/","archives/proxy/","archives/scope-chain/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
